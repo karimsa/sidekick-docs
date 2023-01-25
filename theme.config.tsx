9a0ebff6ff7a9205f2f643ce8ffbdec20f5e6638
Karim Alibhai <karim@alibhai.co>
2023-01-25 03:07:25 -0500

    initial

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Sidekick</span>,
+  project: {
+    link: 'https://github.com/karimsa/sidekick',
+  },
+  docsRepositoryBase: 'https://github.com/karimsa/sidekick',
+  footer: {
+    text: 'Copyright © 2021-present Karim Alibhai',
+  },
+}
+
+export default config
