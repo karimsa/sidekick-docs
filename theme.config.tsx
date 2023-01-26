6de2137fe0a4b9c00a4db5890170db33582b015c
Karim Alibhai <karim@alibhai.co>
2023-01-26 03:41:12 -0500

    add more theme options

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>Sidekick</span>,
-  project: {
-    link: 'https://github.com/karimsa/sidekick',
-  },
-  docsRepositoryBase: 'https://github.com/karimsa/sidekick',
-  footer: {
-    text: 'Copyright © 2021-present Karim Alibhai',
-  },
-}
+	logo: <span>Sidekick</span>,
+	project: {
+		link: "https://github.com/karimsa/sidekick",
+	},
+	docsRepositoryBase: "https://github.com/karimsa/sidekick",
+	footer: {
+		text: "Copyright © 2021-present Karim Alibhai",
+	},
+	useNextSeoProps() {
+		return {
+			titleTemplate: "%s - Sidekick",
+		};
+	},
+	navbar: {
+		extraContent: (
+			<>
+				<a href={"https://npmjs.org/@karimsa/sidekick"}>
+					<img
+						src={
+							"https://img.shields.io/npm/v/@karimsa/sidekick?label=latest&color=green"
+						}
+						alt="latest npm version"
+					/>
+				</a>
+			</>
+		),
+	},
+};
 
-export default config
+export default config;
